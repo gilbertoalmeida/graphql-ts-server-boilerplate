ae808eb46d934c58e43193d0d2b7d634415532c6
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-05-02 02:57:49 +0200

    Saving a user to the database and hashing their password

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,9 @@
-/* import { IResolvers } from "graphql-tools"; */
-import { ResolverMap } from "./types/graphql-utils";
+import { IResolvers } from "graphql-tools";
+import * as bcrypt from "bcryptjs";
+import { User } from "./entity/User";
 
 /* IResolvers is getting types to ad. For the _ , for example */
-export const resolvers: ResolverMap = {
+export const resolvers: IResolvers = {
   Query: {
     /* This GQL.I... thing are the types of the object. The object is in the schema
     And we get the types through the library gql2ts. Run the script in package.json
@@ -11,8 +12,22 @@ export const resolvers: ResolverMap = {
       `Hello ${name || "World"}`
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
-      console.log(email, password);
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword
+      });
+
+      /* Hovering over this function you can see that it returns a Promisse,
+      so you can use await here. If you do the same above at create, it just
+      returns an object for the user, so you cannot do the same */
+      await user.save();
+
+      return true;
     }
   }
 };
